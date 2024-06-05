6bd065db827309655877562939f6a5f6eb363ae6
Ederson <60006824+EdersonPalma@users.noreply.github.com>
2024-06-05 17:15:04 -0300

    teste

--- src/Boleto/Render/view/boleto.blade.php
+++ src/Boleto/Render/view/boleto.blade.php
@@ -1,14 +1,13 @@
 @extends('BoletoHtmlRender::layout')
 @section('boleto')
-
-    @foreach($boletos as $i => $boleto)
+    @foreach ($boletos as $i => $boleto)
         @php extract($boleto, EXTR_OVERWRITE); @endphp
-        @if($mostrar_instrucoes)
+        @if ($mostrar_instrucoes)
             <div class="noprint info">
                 <h2>Instruções de Impressão</h2>
                 <ul>
                     @forelse ($instrucoes_impressao as $instrucao_impressao)
-                        <li>{{ $instrucao_impressao }}</li>
+                        <li class="conteudo">{{ $instrucao_impressao }}</li>
                     @empty
 
                         <li>Imprima em impressora jato de tinta (ink jet) ou laser em qualidade normal ou alta (Não use
@@ -38,7 +37,7 @@
         <div class="info-empresa">
             @if ($logo)
                 <div style="display: inline-block;">
-                    <img alt="logo" src="{{ $logo_base64 }}"/>
+                    <img alt="logo" src="{{ $logo_base64 }}" />
                 </div>
             @endif
             <div style="display: inline-block; vertical-align: super;">
@@ -52,118 +51,118 @@
 
         <table class="table-boleto" cellpadding="0" cellspacing="0" border="0">
             <tbody>
-            <tr>
-                <td valign="bottom" colspan="8" class="noborder nopadding">
-                    <div class="logocontainer">
-                        <div class="logobanco">
-                            <img src="{{ isset($logo_banco_base64) && !empty($logo_banco_base64) ? $logo_banco_base64 : 'https://dummyimage.com/150x75/fff/000000.jpg&text=+' }}"
-                                 alt="logo do banco">
+                <tr>
+                    <td valign="bottom" colspan="8" class="noborder nopadding">
+                        <div class="logocontainer">
+                            <div class="logobanco">
+                                <img src="{{ isset($logo_banco_base64) && !empty($logo_banco_base64) ? $logo_banco_base64 : 'https://dummyimage.com/150x75/fff/000000.jpg&text=+' }}"
+                                    alt="logo do banco">
+                            </div>
+                            <div class="codbanco">{{ $codigo_banco_com_dv }}</div>
                         </div>
-                        <div class="codbanco">{{ $codigo_banco_com_dv }}</div>
-                    </div>
-                    <div class="linha-digitavel">{{ $linha_digitavel }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" width="250" class="top-2">
-                    <div class="titulo">Beneficiário</div>
-                    <div class="conteudo">{{ $beneficiario['nome'] }}</div>
-                </td>
-                <td class="top-2">
-                    <div class="titulo">CPF/CNPJ</div>
-                    <div class="conteudo">{{ $beneficiario['documento'] }}</div>
-                </td>
-                <td width="120" class="top-2">
-                    <div class="titulo">Ag/Cod. Beneficiário</div>
-                    <div class="conteudo rtl">{{ $agencia_codigo_beneficiario }}</div>
-                </td>
-                <td width="120" class="top-2">
-                    <div class="titulo">Vencimento</div>
-                    <div class="conteudo rtl">{{ $data_vencimento->format('d/m/Y') }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <div class="titulo">Pagador</div>
-                    <div class="conteudo">{{ $pagador['nome_documento'] }} </div>
-                </td>
-                <td>
-                    <div class="titulo">Nº documento</div>
-                    <div class="conteudo rtl">{{ $numero_documento }}</div>
-                </td>
-                <td>
-                    <div class="titulo">Nosso número</div>
-                    <div class="conteudo rtl">{{ $nosso_numero_boleto }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="titulo">Espécie</div>
-                    <div class="conteudo">{{ $especie }}</div>
-                </td>
-                <td>
-                    <div class="titulo">Quantidade</div>
-                    <div class="conteudo rtl"></div>
-                </td>
-                <td>
-                    <div class="titulo">Valor</div>
-                    <div class="conteudo rtl"></div>
-                </td>
-                <td>
-                    <div class="titulo">(-) Descontos / Abatimentos</div>
-                    <div class="conteudo rtl"></div>
-                </td>
-                <td>
-                    <div class="titulo">(=) Valor Documento</div>
-                    <div class="conteudo rtl">{{ $valor }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <div class="conteudo"></div>
-                    <div class="titulo">Demonstrativo</div>
-                </td>
-                <td>
-                    <div class="titulo">(-) Outras deduções</div>
-                    <div class="conteudo"></div>
-                </td>
-                <td>
-                    <div class="titulo">(+) Outros acréscimos</div>
-                    <div class="conteudo rtl"></div>
-                </td>
-                <td>
-                    <div class="titulo">(=) Valor cobrado</div>
-                    <div class="conteudo rtl"></div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="4">
-                    <div style="margin-top: 10px" class="conteudo">{{ $demonstrativo[0] }}</div>
-                </td>
-                <td class="noleftborder">
-                    <div class="titulo">Autenticação mecânica</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="5" class="notopborder">
-                    <div class="conteudo">{{ $demonstrativo[1] }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="5" class="notopborder">
-                    <div class="conteudo">{{ $demonstrativo[2] }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="5" class="notopborder">
-                    <div class="conteudo">{{ $demonstrativo[3] }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="5" class="notopborder bottomborder">
-                    <div style="margin-bottom: 10px;" class="conteudo">{{ $demonstrativo[4] }}</div>
-                </td>
-            </tr>
+                        <div class="linha-digitavel">{{ $linha_digitavel }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" width="250" class="top-2">
+                        <div class="titulo">Beneficiário</div>
+                        <div class="conteudo">{{ $beneficiario['nome'] }}</div>
+                    </td>
+                    <td class="top-2">
+                        <div class="titulo">CPF/CNPJ</div>
+                        <div class="conteudo">{{ $beneficiario['documento'] }}</div>
+                    </td>
+                    <td width="120" class="top-2">
+                        <div class="titulo">Ag/Cod. Beneficiário</div>
+                        <div class="conteudo rtl">{{ $agencia_codigo_beneficiario }}</div>
+                    </td>
+                    <td width="120" class="top-2">
+                        <div class="titulo">Vencimento</div>
+                        <div class="conteudo rtl">{{ $data_vencimento->format('d/m/Y') }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <div class="titulo">Pagador</div>
+                        <div class="conteudo">{{ $pagador['nome_documento'] }} </div>
+                    </td>
+                    <td>
+                        <div class="titulo">Nº documento</div>
+                        <div class="conteudo rtl">{{ $numero_documento }}</div>
+                    </td>
+                    <td>
+                        <div class="titulo">Nosso número</div>
+                        <div class="conteudo rtl">{{ $nosso_numero_boleto }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="titulo">Espécie</div>
+                        <div class="conteudo">{{ $especie }}</div>
+                    </td>
+                    <td>
+                        <div class="titulo">Quantidade</div>
+                        <div class="conteudo rtl"></div>
+                    </td>
+                    <td>
+                        <div class="titulo">Valor</div>
+                        <div class="conteudo rtl"></div>
+                    </td>
+                    <td>
+                        <div class="titulo">(-) Descontos / Abatimentos</div>
+                        <div class="conteudo rtl"></div>
+                    </td>
+                    <td>
+                        <div class="titulo">(=) Valor Documento</div>
+                        <div class="conteudo rtl">{{ $valor }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div class="conteudo"></div>
+                        <div class="titulo">Demonstrativo</div>
+                    </td>
+                    <td>
+                        <div class="titulo">(-) Outras deduções</div>
+                        <div class="conteudo"></div>
+                    </td>
+                    <td>
+                        <div class="titulo">(+) Outros acréscimos</div>
+                        <div class="conteudo rtl"></div>
+                    </td>
+                    <td>
+                        <div class="titulo">(=) Valor cobrado</div>
+                        <div class="conteudo rtl"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <div style="margin-top: 10px" class="conteudo">{{ $demonstrativo[0] }}</div>
+                    </td>
+                    <td class="noleftborder">
+                        <div class="titulo">Autenticação mecânica</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="notopborder">
+                        <div class="conteudo">{{ $demonstrativo[1] }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="notopborder">
+                        <div class="conteudo">{{ $demonstrativo[2] }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="notopborder">
+                        <div class="conteudo">{{ $demonstrativo[3] }}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="notopborder bottomborder">
+                        <div style="margin-bottom: 10px;" class="conteudo">{{ $demonstrativo[4] }}</div>
+                    </td>
+                </tr>
             </tbody>
         </table>
         <br>
@@ -173,7 +172,7 @@
         <!-- Ficha de compensação -->
         @include('BoletoHtmlRender::partials/ficha-compensacao')
 
-        @if(count($boletos) > 1 && count($boletos)-1 != $i)
+        @if (count($boletos) > 1 && count($boletos) - 1 != $i)
             <div style="page-break-before:always"></div>
         @endif
     @endforeach
